488015722970d0f1cefa8e131151b5cf70a0980e
陈绍斌 <1748362428@qq.com>
2023-12-27 11:08:25 +0800

    docs(测试修改eslint): test eslint again

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
  * @Author: 陈绍斌 1748362428@qq.com
  * @Date: 2023-12-26 10:43:35
  * @LastEditors: 陈绍斌 1748362428@qq.com
- * @LastEditTime: 2023-12-26 15:54:00
+ * @LastEditTime: 2023-12-26 16:53:29
  * @FilePath: \Babel\src\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
